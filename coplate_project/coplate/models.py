db3e39de79601989381acddc61902882551f3de6
SeolHuiGwan9478 <shg5977@naver.com>
2022-01-13 23:17:51 +0900

    setup allauth package

--- coplate_project/coplate/models.py
+++ coplate_project/coplate/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractModel
+from django.contrib.auth.models import AbstractUser
 
-class User(AbstractModel):
+class User(AbstractUser):
     pass
 # Create your models here.
