569e2e461cb4998c876a51ad6830a6a125504057
SeolHuiGwan9478 <shg5977@naver.com>
2022-01-17 19:46:31 +0900

    use django-widget-tweaks

--- coplate_project/coplate_project/settings.py
+++ coplate_project/coplate_project/settings.py
@@ -38,11 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-
+    'coplate',
+    'widget_tweaks',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'coplate',
 ]
 
 
