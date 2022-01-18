be22f96ce280b8a16fd874c27dced142b7b58e21
SeolHuiGwan9478 <shg5977@naver.com>
2022-01-19 01:08:19 +0900

    modify index design

--- coplate_project/coplate/views.py
+++ coplate_project/coplate/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.urls import reverse
 from allauth.account.views import PasswordChangeView
+from django.views.generic import ListView
+from coplate.models import Review
 
 # Create your views here.
-def index(request):
-    print(request.user)
-    return render(request, 'coplate/index.html')
+class IndexView(ListView):
+    model = Review
+    template_name = "coplate/index.html"
+    context_object_name = "reviews"
+    paginate_by = 4
+    ordering = ["-dt_created"]
 
 class CustomPasswordChangeView(PasswordChangeView):
     def get_success_url(self):
--- coplate_project/coplate_project/settings.py
+++ coplate_project/coplate_project/settings.py
@@ -115,9 +115,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/uploads/'
+STATIC_URL = "/static/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/uploads/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
