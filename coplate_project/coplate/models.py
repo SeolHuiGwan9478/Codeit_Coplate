1f0a906d6abefdfb1ca27c807ef500a98995faa6
SeolHuiGwan9478 <shg5977@naver.com>
2022-01-18 12:55:20 +0900

    add media folder & modify settings.py for static files

--- coplate_project/coplate/models.py
+++ coplate_project/coplate/models.py
@@ -10,3 +10,27 @@ class User(AbstractUser):
     def __str__(self):
         return self.email
 # Create your models here.
+class Review(models.Model):
+    title = models.CharField(max_length=30)
+    restaurant_name = models.CharField(max_length=20)
+    restaurant_link = models.URLField(validators=[validate_restaurant_link])
+
+    RATING_CHOICES = [
+        (1,1),
+        (2,2),
+        (3,3),
+        (4,4),
+        (5,5),
+    ]
+
+    rating = models.IntegerField(choices=RATING_CHOICES)
+
+    image1 = models.ImageField()
+    image2 = models.ImageField()
+    image3 = models.ImageField()
+    content = models.TextField()
+    dt_created = models.DateTimeField(auto_now_add=True)
+    dt_updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
