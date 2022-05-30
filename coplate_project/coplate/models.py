c7f8dc1f7853769ae4480509921365003050c76e
SeolHuiGwan9478 <shg5977@naver.com>
2022-05-30 11:51:32 +0900

    [feat] : add Like model by using GenericView

--- coplate_project/coplate/models.py
+++ coplate_project/coplate/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from .validators import validate_no_special_characters, validate_restaurant_link
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 class User(AbstractUser):
     nickname = models.CharField(max_length = 15, unique=True, null=True,
         error_messages = {"unique":"이미 사용중인 닉네임입니다."},
@@ -9,7 +11,7 @@ class User(AbstractUser):
 
     profile_pic = models.ImageField(upload_to="profile_pics", default="default_profile_pic.jpg")
     intro = models.CharField(max_length=60, blank=True)
-
+    following = models.ManyToManyField('self', symmetrical=False)
     def __str__(self):
         return self.email
 # Create your models here.
@@ -39,3 +41,23 @@ class Review(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    content = models.TextField
+    dt_created = models.DateTimeField(auto_now_add=True)
+    dt_updated = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content[:30]
+
+class Like(models.Model):
+    dt_created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    liked_object = GenericForeignKey()
+
+    def __str__(self):
+        return f"({self.user}, {self.liked_object})"
